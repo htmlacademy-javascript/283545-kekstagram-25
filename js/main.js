496d5c8f8afa849710e843b929bdaa190db9d964
alexanderkuzmenko <57828759+Haarkin@users.noreply.github.com>
2022-02-24 15:44:59 +0200

    Делает функцию выбора числа

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-const randomNumber = (first, second) => Math.random(first, second);
+// Выбор случайного числа
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Проверка строки
 const inspectString = (string, maxWidth = 140) => string.length < maxWidth;
